07d3d53d14b287a8f183747b6708aa1c384b8ea4
Jason Stumpf <jason.stumpf@gmail.com>
2017-03-28 15:32:39 -0700

    Check now returns the failing arguments.

--- check.hh
+++ check.hh
@@ -3,29 +3,32 @@
 
 #include <cassert>
 #include <functional>
+#include <experimental/optional>
+#include <tuple>
 #include <type_traits>
 
 #include "special.hh"
 
-inline bool check(std::function<bool()> func) {
+inline std::experimental::optional<std::tuple<>> check(std::function<bool()> func) {
   assert(bool(func));
-  return func();
+  return func() ? std::experimental::optional<std::tuple<>>{} : std::tuple<>{};
 }
 
 template <typename arg, typename... args>
-inline bool check(std::function<bool(arg, args...)> func) {
+inline std::experimental::optional<std::tuple<arg, args...>> check(std::function<bool(arg, args...)> func) {
   assert(bool(func));
   using dec_arg = typename std::decay<arg>::type;
   for(dec_arg const & val : special<dec_arg>) {
-    if(!check(std::function<bool(args const &...)>{
-           [&](args const &... a) { return func(val, a...); }})) {
-      return false;
+    std::experimental::optional<std::tuple<args...>> result(check(std::function<bool(args const &...)>{
+           [&](args const &... a) { return func(val, a...); }}));
+    if(result) {
+      return std::tuple_cat(std::tuple<arg>{val}, *result);
     }
   }
-  return true;
+  return {};
 }
 
-template <typename... args> inline bool check(bool (&func)(args...)) {
+template <typename... args> inline std::experimental::optional<std::tuple<args...>> check(bool (&func)(args...)) {
   return check(std::function<bool(args...)>(func));
 }
 
