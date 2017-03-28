7e6a19f1e0bc8e685e9f386bb5f7c5166e986b16
Jason Stumpf <jason.stumpf@gmail.com>
2017-03-28 16:38:42 -0700

    Formatting.

--- check.hh
+++ check.hh
@@ -2,25 +2,28 @@
 #define CHECK_HH
 
 #include <cassert>
-#include <functional>
 #include <experimental/optional>
+#include <functional>
 #include <tuple>
 #include <type_traits>
 
 #include "special.hh"
 
-inline std::experimental::optional<std::tuple<>> check(std::function<bool()> func) {
+inline std::experimental::optional<std::tuple<>>
+    check(std::function<bool()> func) {
   assert(bool(func));
   return func() ? std::experimental::optional<std::tuple<>>{} : std::tuple<>{};
 }
 
 template <typename arg, typename... args>
-inline std::experimental::optional<std::tuple<arg, args...>> check(std::function<bool(arg, args...)> func) {
+inline std::experimental::optional<std::tuple<arg, args...>>
+    check(std::function<bool(arg, args...)> func) {
   assert(bool(func));
   using dec_arg = typename std::decay<arg>::type;
   for(dec_arg const & val : special<dec_arg>) {
-    std::experimental::optional<std::tuple<args...>> result(check(std::function<bool(args const &...)>{
-           [&](args const &... a) { return func(val, a...); }}));
+    std::experimental::optional<std::tuple<args...>> result(
+        check(std::function<bool(args const &...)>{
+            [&](args const &... a) { return func(val, a...); }}));
     if(result) {
       return std::tuple_cat(std::tuple<arg>{val}, *result);
     }
@@ -28,7 +31,9 @@ inline std::experimental::optional<std::tuple<arg, args...>> check(std::function
   return {};
 }
 
-template <typename... args> inline std::experimental::optional<std::tuple<args...>> check(bool (&func)(args...)) {
+template <typename... args>
+inline std::experimental::optional<std::tuple<args...>>
+    check(bool (&func)(args...)) {
   return check(std::function<bool(args...)>(func));
 }
 
