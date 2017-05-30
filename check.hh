09d08d022fe24ff782d558b4345e25220be16e01
Jason Stumpf <jason.stumpf@gmail.com>
2017-05-30 16:48:35 -0700

    Removed inline.

--- check.hh
+++ check.hh
@@ -12,14 +12,14 @@
 #include "special.hh"
 
 template <typename... givens> struct given {
-  static inline std::experimental::optional<std::tuple<givens...>>
+  static std::experimental::optional<std::tuple<givens...>>
       check_special(bool (&func)(givens...), givens const &... gvals) {
     return func(gvals...) ? std::experimental::optional<std::tuple<givens...>>{}
                           : std::tuple<givens...>{gvals...};
   }
 
   template <typename arg, typename... args>
-  static inline std::experimental::optional<std::tuple<givens..., arg, args...>>
+  static std::experimental::optional<std::tuple<givens..., arg, args...>>
       check_special(
           bool (&func)(givens..., arg, args...), givens const &... gvals) {
     using dec_arg = typename std::decay<arg>::type;
@@ -32,7 +32,7 @@ template <typename... givens> struct given {
     return {};
   }
 
-  static inline std::experimental::optional<std::tuple<givens...>> check_small(
+  static std::experimental::optional<std::tuple<givens...>> check_small(
       std::default_random_engine &,
       bool (&func)(givens...),
       givens const &... gvals) {
@@ -41,7 +41,7 @@ template <typename... givens> struct given {
   }
 
   template <typename arg, typename... args>
-  static inline std::experimental::optional<std::tuple<givens..., arg, args...>>
+  static std::experimental::optional<std::tuple<givens..., arg, args...>>
       check_small(
           std::default_random_engine & gen,
           bool (&func)(givens..., arg, args...),
@@ -57,7 +57,7 @@ template <typename... givens> struct given {
     return {};
   }
 
-  static inline std::experimental::optional<std::tuple<givens...>> check_large(
+  static std::experimental::optional<std::tuple<givens...>> check_large(
       std::default_random_engine &,
       bool (&func)(givens...),
       givens const &... gvals) {
@@ -66,7 +66,7 @@ template <typename... givens> struct given {
   }
 
   template <typename arg, typename... args>
-  static inline std::experimental::optional<std::tuple<givens..., arg, args...>>
+  static std::experimental::optional<std::tuple<givens..., arg, args...>>
       check_large(
           std::default_random_engine & gen,
           bool (&func)(givens..., arg, args...),
@@ -83,15 +83,15 @@ template <typename... givens> struct given {
   }
 };
 
-inline void print_args() {}
+ void print_args() {}
 
 template <typename arg, typename... args>
-inline void print_args(arg const & val, args const &... vals) {
+ void print_args(arg const & val, args const &... vals) {
   std::cerr << val << "\n";
   print_args(vals...);
 }
 
-template <typename... args> inline void test(bool (&func)(args...)) {
+template <typename... args>  void test(bool (&func)(args...)) {
   auto result(given<>::check_special(func));
   if(result) {
     std::cerr << "FAILURE\n";
