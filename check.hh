2b466d2cb040a8922ba12b884c2a61ced9359ead
Jason Stumpf <jason.stumpf@gmail.com>
2017-06-01 17:59:40 -0700

    Rewrite of check that's significantly simpler.

--- check.hh
+++ check.hh
@@ -11,51 +11,46 @@
 #include "generate.hh"
 #include "special.hh"
 
-template <typename... givens> struct given {
-  static std::experimental::optional<std::tuple<givens...>>
-      check_special(bool (&func)(givens...), givens const &... gvals) {
-    return func(gvals...) ? std::experimental::optional<std::tuple<givens...>>{}
-                          : std::tuple<givens...>{gvals...};
-  }
+template <typename... args>
+std::experimental::optional<std::tuple<args...>>
+    test_with(bool (&func)(args...), args const &... vals) {
+  return func(vals...) ? std::experimental::optional<std::tuple<args...>>{}
+                       : std::tuple<args...>{vals...};
+}
 
-  template <typename arg, typename... args>
-  static std::experimental::optional<std::tuple<givens..., arg, args...>>
-      check_special(
-          bool (&func)(givens..., arg, args...), givens const &... gvals) {
-    using dec_arg = typename std::decay<arg>::type;
-    for(dec_arg const & val : special<dec_arg>) {
-      auto result(given<givens..., arg>::check_special(func, gvals..., val));
-      if(result) {
-        return result;
-      }
+template <typename T> T gen(std::default_random_engine & eng, short size) {
+  if(size == 0) {
+    unsigned special_size =
+        static_cast<unsigned>(special<typename std::decay<T>::type>.size());
+    if(special_size == 0) {
+      return generate<typename std::decay<T>::type>(eng, 0);
     }
-    return {};
+    unsigned index =
+        std::uniform_int_distribution<unsigned>{0, special_size - 1}(eng);
+    return special<typename std::decay<T>::type>[index];
+  } else {
+    return generate<typename std::decay<T>::type>(eng, size - 1);
   }
+}
 
-  static std::experimental::optional<std::tuple<givens...>> check(
-      std::default_random_engine &,
-      bool (&func)(givens...),
-      givens const &... gvals) {
-    return func(gvals...) ? std::experimental::optional<std::tuple<givens...>>{}
-                          : std::tuple<givens...>{gvals...};
-  }
+template <typename... args>
+std::experimental::optional<std::tuple<args...>> test_at(
+    std::default_random_engine & eng, bool (&func)(args...), short size) {
+  return test_with<args...>(
+      func, gen<typename std::decay<args>::type>(eng, size)...);
+}
 
-  template <typename arg, typename... args>
-  static std::experimental::optional<std::tuple<givens..., arg, args...>> check(
-      std::default_random_engine & gen,
-      bool (&func)(givens..., arg, args...),
-      givens const &... gvals) {
-    using dec_arg = typename std::decay<arg>::type;
-    for(short i = 0; i < 1000; ++i) {
-      auto result(given<givens..., arg>::check(
-          gen, func, gvals..., generate<dec_arg>(gen, i / 10)));
-      if(result) {
-        return result;
-      }
+template <typename... args>
+std::experimental::optional<std::tuple<args...>>
+    test_all(std::default_random_engine & eng, bool (&func)(args...)) {
+  for(short size = 0; size < 1000; ++size) {
+    auto result = test_at(eng, func, size / 10);
+    if(result) {
+      return result;
     }
-    return {};
   }
-};
+  return {};
+}
 
 void print_args() {}
 
@@ -66,19 +61,13 @@ void print_args(arg const & val, args const &... vals) {
 }
 
 template <typename... args> void test(bool (&func)(args...)) {
-  auto result(given<>::check_special(func));
+  std::default_random_engine gen{};
+  auto result(test_all(gen, func));
   if(result) {
     std::cerr << "FAILURE\n";
     std::experimental::apply(print_args<args...>, *result);
   } else {
-    std::default_random_engine gen{};
-    auto result_small(given<>::check(gen, func));
-    if(result_small) {
-      std::cerr << "FAILURE\n";
-      std::experimental::apply(print_args<args...>, *result_small);
-    } else {
-      std::cerr << "SUCCESS\n";
-    }
+    std::cerr << "SUCCESS\n";
   }
 }
 
